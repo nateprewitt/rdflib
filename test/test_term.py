0f5a9de67847789b562ad2924f27253f4e14748e
Nate Prewitt <Nate.Prewitt@gmail.com>
2016-12-02 16:47:12 -0700

    test base64 isn't converted

--- test/test_term.py
+++ test/test_term.py
@@ -4,8 +4,9 @@ some more specific Literal tests are in test_literal.py
 
 import unittest
 from rdflib.py3compat import format_doctest_out as uformat
-from rdflib.term import URIRef, BNode
+from rdflib.term import URIRef, BNode, Literal
 from rdflib.graph import QuotedGraph, Graph
+from rdflib.namespace import XSD
 
 class TestURIRefRepr(unittest.TestCase):
     """
@@ -25,15 +26,23 @@ class TestURIRefRepr(unittest.TestCase):
         a = u>BNode()
         a = u>QuotedGraph(g.store, u)
         a = u>g
-        
-        
-        
-        
+
 
 class TestBNodeRepr(unittest.TestCase):
-   
+
     def testSubclassNameAppearsInRepr(self):
         class MyBNode(BNode):
             pass
         x = MyBNode()
         self.assert_(repr(x).startswith("MyBNode("))
+
+
+class TestXSDToPythonTypes(unittest.TestCase):
+
+    def test_base64_isnt_decoded(self):
+        """
+        ensure base64 values aren't decoded to avoid accidental data mangling.
+        """
+        b64msg = 'cmRmbGliIGlzIGNvb2whIGFsc28gaGVyZSdzIHNvbWUgYmluYXJ5IAAR83UC'
+        lit = Literal(b64msg, datatype=XSD.base64Binary)
+        self.assertEqual(lit.value, b64msg)
